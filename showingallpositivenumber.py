eae69ecbf30d414741422a65d3e84a8928459ca9
Adityanarayanojha <90720550+Adityanarayanojha@users.noreply.github.com>
2022-01-03 10:11:39 +0530

    Create showingallpositivenumber.py

---
+++ showingallpositivenumber.py
@@ -0,0 +1,22 @@
+list1 = [12, -7, 5, 64 , -14]
+
+print("list1: ",list1)
+print("output ")
+  
+for num in list1:
+      
+    if num >= 0:
+       print(num, end = ", ")
+       
+       
+list2 = [12, 14, -95 , 3]
+print(" ")
+
+print("list2: ",list2)
+print("output ")
+
+for num in list2:
+      
+    if num >= 0:
+       print(num,end = " ")   
+       
